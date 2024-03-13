ed5e915b096275f51488b5b575011726e9abb2d1
danny ong <danny.ong@fdmgroup.com>
2024-03-13 10:36:29 +0800

    uploading orderwebsite project

--- src/main/java/fdmgroup/OrderWebsite/service/MenuService.java
+++ src/main/java/fdmgroup/OrderWebsite/service/MenuService.java
@@ -12,6 +12,10 @@ import fdmgroup.OrderWebsite.model.store.Topping;
 import fdmgroup.OrderWebsite.repository.DrinkRepository;
 import fdmgroup.OrderWebsite.repository.ToppingRepository;
 
+
+/**
+ * Service class responsible for managing the menu, including drinks and toppings.
+ */
 @Service("menuService")
 public class MenuService {
 	
@@ -30,14 +34,28 @@ public class MenuService {
 	
 	private static final Logger log = LogManager.getLogger(MenuService.class);
 	
+	
+	/**
+     * Retrieves the list of available drinks.
+     * @return List of Drink objects representing the menu.
+     */
 	public List<Drink> getDrinks() {
 		return drinkRepo.findAll();
 	}
 	
+	
+	/**
+     * Retrieves the list of available toppings.
+     * @return List of Topping objects representing the menu.
+     */
 	public List<Topping> getToppings(){
 		return toppingRepo.findAll();
 	}
 	
+	
+	/**
+     * Initializes the menu by populating it with drinks and toppings if they are empty.
+     */
 	public void initialiseMenu() {
 		if(getToppings().isEmpty()) {
 			log.info("ToppingMenuNull: Topping Menu is empty");
